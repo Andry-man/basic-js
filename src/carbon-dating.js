9c7d13841f2e1c8e4a0f637d87b3e60a8b436920
Andrei Chapiuk <venik2281488@gmail.ru>
2020-02-29 19:25:15 +0300

    complete: complate count cats-task

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,7 +1,9 @@
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
+module.exports = function dateSample(sampleActivity) {
+var k = (0.693/5700)*1.22*10**4;
+var t = (15/sampleActivity)/k*(15/8.0)/(1.22*10**4)*5.2*10**3;
+return k ;
+ 
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
 };
--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,13 @@
-module.exports = function countCats(/* matrix */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function countCats(m) {
+  let matrix = m;
+  let answer = 0; 
+  for (let i = 0; i < matrix.length; i++) {
+  for (let index = 0; index < matrix[i].length; index++) {
+   if (matrix[i][index] == '^^' ) {
+   answer+=1
+   }
+    
+  }
+  }
+ return answer;
 };
